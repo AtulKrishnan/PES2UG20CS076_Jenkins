ed406cdec14ac6210717944c5f73c823506c06a1
Atul Krishnan <79460392+AtulKrishnan@users.noreply.github.com>
2023-02-18 19:29:18 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG20CS076, Atul Krishnan, TASK IS DONE\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
